8b457d527e924a589f76f0c2b5759a4f6a817007
Caua Caetano <cauacaetanofb@gmail.com>
2026-04-01 15:21:17 -0300

    FIX: Registra modelo User no Django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,2 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, PasswordResetCode
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'user_type', 'is_active')
+    list_filter = ('user_type', 'is_active', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'cpf')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações Extras', {
+            'fields': ('user_type', 'cpf', 'telefone'),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Informações Extras', {
+            'fields': ('user_type', 'cpf', 'telefone'),
+        }),
+    )
+
+
+@admin.register(PasswordResetCode)
+class PasswordResetCodeAdmin(admin.ModelAdmin):
+    list_display = ('email', 'code', 'criado_em', 'usado')
+    list_filter = ('usado',)
+    search_fields = ('email',)
